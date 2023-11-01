2d1368d4b1ac33d04f178a1dffe0724e3a57be7f
Darius Baciu <baciu.darius01@gmail.com>
2023-11-01 16:06:39 +0200

    children and parents pages

--- public/wp-content/w3tc-config/master.php
+++ public/wp-content/w3tc-config/master.php
@@ -1004,7 +1004,7 @@
             ]
         }
     },
-    "common.track_usage": false,
+    "common.track_usage": true,
     "common.tweeted": false,
     "config.check": true,
     "config.path": "",
