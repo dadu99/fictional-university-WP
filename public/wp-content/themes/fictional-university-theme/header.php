9811b76f412045156a9dea6089c45a1341a5cc35
Darius Baciu <baciu.darius01@gmail.com>
2023-11-07 12:15:18 +0200

    changes blog improvements

--- public/wp-content/themes/fictional-university-theme/header.php
+++ public/wp-content/themes/fictional-university-theme/header.php
@@ -23,18 +23,22 @@
           -->
 
           <ul>
-            <li <?php if (is_page('about-us') or wp_get_post_parent_id(get_the_ID(0) == 13)) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+            <li <?php if (is_page('about-us') or wp_get_post_parent_id(get_the_ID(0) == 13)) echo 'class="current-menu-item"' ?>>
+              <a href="<?php echo site_url('/about-us') ?>">About Us</a>
+            </li>
             <li><a href="#">Programs</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
-            <li><a href="#">Blog</a></li>
+            <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>>
+              <a href="<?php echo site_url('/blog'); ?>">Blog</a>
+            </li>
           </ul>
         </nav>
-        <div class="site-header__util">
+        <!-- <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
           <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
           <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
-        </div>
+        </div> -->
       </div>
     </div>
   </header>
